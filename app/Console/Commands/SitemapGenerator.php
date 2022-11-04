f62bfc38717695d74a3c0904d0d78fc1d019e955
Alex Gavrilov <alexgavrilov939@gmail.com>
2022-11-05 01:23:29 +0300

    WEBAPP-0: logic reworked to use composer library.

--- app/Console/Commands/SitemapGenerator.php
+++ app/Console/Commands/SitemapGenerator.php
@@ -2,21 +2,20 @@
 
 namespace App\Console\Commands;
 
-use App\Console\Commands\Exceptions\FileCreatingException;
-use App\Console\Commands\Exceptions\UnexceptedFileTypeException;
-use App\Console\Commands\Exceptions\UnexceptedResultException;
 use Illuminate\Console\Command;
-use SimpleXMLElement;
+use Sitemap\Generator as CustomSitemapGenerator;
 
 class SitemapGenerator extends Command
 {
+    /**
+     * @var string
+     */
     protected $signature = 'sitemap:generate {--file_type=xml} {--file_path=}';
 
+    /**
+     * @var string
+     */
     protected $description = 'Generates sitemap files in xml, csv, json formats.';
-
-    const SITEMAP_FILE_TYPE__XML = 'xml';
-    const SITEMAP_FILE_TYPE__CSV = 'csv';
-    const SITEMAP_FILE_TYPE__JSON = 'json';
 
     const PAGES_DATA = [
         [
@@ -57,11 +56,11 @@ class SitemapGenerator extends Command
         ]
     ];
 
-    /**
-     * @throws UnexceptedFileTypeException
-     * @throws UnexceptedResultException
-     * @throws FileCreatingException
-     */
+    public function __construct(private readonly CustomSitemapGenerator $sitemapGenerator)
+    {
+        parent::__construct();
+    }
+
     public function handle()
     {
         $sitemapFileType = $this->option('file_type');
@@ -72,34 +71,19 @@ class SitemapGenerator extends Command
 
         $data = self::PAGES_DATA;
         $sitemapFilename = 'sitemap_'.date('Y-m-d').".{$sitemapFileType}";
-        $this->prettylog("Getting ready to generate the file {$sitemapFilename}");
-        $this->prettylog('Found '.count($data).' items. Start processing...');
+        $this->prettyLog("Getting ready to generate the file {$sitemapFilename}");
+        $this->prettyLog('Found '.count($data).' items. Start processing...');
 
-        if ($sitemapFileType === self::SITEMAP_FILE_TYPE__XML) {
-            $content = $this->generateXmlFromArray(self::PAGES_DATA);
-        } elseif ($sitemapFileType === self::SITEMAP_FILE_TYPE__JSON) {
-            $content = $this->generateJsonFromArray(self::PAGES_DATA);
-        } elseif ($sitemapFileType === self::SITEMAP_FILE_TYPE__CSV) {
-            $content = $this->generateCsvFromArray(self::PAGES_DATA, ';');
-        } else {
-            $this->prettylog('Oops something unexpected happened :(', 'error');
-            throw new UnexceptedFileTypeException();
+        try {
+            $sitemapFile = $this->sitemapGenerator->generateSitemap(self::PAGES_DATA, $sitemapFileType, $sitemapFilePath, $sitemapFilename);
+            $this->prettyLog("File is ready by path: {$sitemapFile}");
+            $this->prettyLog('Completed ✓ You are excellent: )', 'success');
+        } catch (\Throwable $e) {
+            $this->prettyLog('Oops something unexpected happened :( '. $e->getMessage(), 'error');
         }
-
-        if (! file_exists($sitemapFilePath)) {
-            mkdir($sitemapFilePath, 0777, true);
-        }
-
-        $sitemapFile = "{$sitemapFilePath}/sitemap.{$sitemapFileType}";
-        $result = file_put_contents($sitemapFile, $content);
-        if (! $result) {
-            throw new FileCreatingException('Unable to create sitemap file', 100503);
-        }
-        $this->prettylog("File is ready by path: {$sitemapFile}");
-        $this->prettylog('Completed ✓ You are excellent: )', 'success');
     }
 
-    private function prettylog($msg, $type = 'default')
+    private function prettyLog($msg, $type = 'default')
     {
         if ($type === 'error') {
             $style = 'fg=red';
@@ -110,74 +94,5 @@ class SitemapGenerator extends Command
         }
 
         $this->line($msg, $style);
-    }
-
-    /**
-     * @param array $data
-     * @return bool|string
-     * @throws UnexceptedResultException
-     */
-    private function generateXmlFromArray(array $data): bool|string
-    {
-        $xml = new SimpleXMLElement("<?xml version='1.0' encoding='UTF-8' ?><urlset xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://www.sitemaps.org/schemas/sitemap/0.9' xsi:schemaLocation='http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd' />");
-        $this->multidimensionalArrayToXml($xml, $data, 'url');
-
-        $xmlStr = $xml->asXML();
-        if (! $xmlStr) {
-            throw new UnexceptedResultException('Unexpected error occurred. Check your file type and try again.', 100501);
-        }
-
-        return $xmlStr;
-    }
-
-    /**
-     * @param array $data
-     * @param string $delimiter
-     * @return string
-     */
-    private function generateCsvFromArray(array $data, string $delimiter=':'): string
-    {
-        $csvStr = '';
-        $titles = array_keys($data[0] ?? []);
-        $csvStr .= implode($delimiter, $titles).PHP_EOL;
-        foreach ($data as $item) {
-            $csvStr .= implode($delimiter, $item).PHP_EOL;
-        }
-
-        return $csvStr;
-    }
-
-    /**
-     * @throws UnexceptedResultException
-     */
-    private function generateJsonFromArray(array $data): string
-    {
-        $jsonStr = json_encode($data, JSON_PRETTY_PRINT);
-        if (! $jsonStr) {
-            throw new UnexceptedResultException('Unexpected error occurred. Check your file type and try again.', 100502);
-        }
-
-        return $jsonStr;
-    }
-
-    /**
-     * @param \SimpleXMLElement $xmlObj
-     * @param array $data
-     * @param string $nodeKey
-     * @return void
-     */
-    private function multidimensionalArrayToXml(SimpleXMLElement $xmlObj, array $data, string $nodeKey = 'item'): void
-    {
-        foreach ($data as $key => $value) {
-            if (is_numeric($key)) {
-                $key = $nodeKey;
-            }
-            if (is_array($value)) {
-                $node = $xmlObj->addChild($key);
-                $this->multidimensionalArrayToXml($node, $value);
-            } else {
-                $xmlObj->addChild($key, htmlspecialchars($value));
-            }
-        }
     }
 }
